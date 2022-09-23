b73094d76b0b6e58880413075d4bcd9e23fa7553
Lingfey <bokanchalangman@gmail.com>
2022-09-23 22:27:59 +0800

    1
    Do... while-loop, if-statement

---
+++ Homework5_1_PasswordCheck/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Homework5_1_PasswordCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "1q2w3e4r", passwordRequest;
+            do
+            {
+                Console.WriteLine("Enter your password");
+                passwordRequest = Console.ReadLine();
+                if (password == passwordRequest)
+                {
+                    Console.WriteLine("Welcome back!");
+                }
+                else if (passwordRequest == "administrator")
+                {
+                    Console.WriteLine("Authorized as administrator.");
+                    break;
+                }
+            }
+            while (password != passwordRequest);
+            Console.ReadKey();
+        }
+    }
+}
